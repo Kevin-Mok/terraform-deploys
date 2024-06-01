38121073178af18b50b21c2e9bcfbc65e5498697
Kevin Mok <kevin.mok@mail.utoronto.ca>
2024-06-01 14:49:50 -0400

    Terraform Docker

--- main.tf
+++ main.tf
@@ -10,7 +10,13 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "pigallery" {
-  name = "bpatrik/pigallery2"
+  name         = "bpatrik/pigallery2"
+  keep_locally = true
+}
+
+resource "docker_image" "hemmelig" {
+  name         = "hemmeligapp/hemmelig"
+  keep_locally = true
 }
 
 resource "docker_container" "pigallery" {
@@ -44,5 +50,28 @@ resource "docker_container" "pigallery" {
     container_path  = "/app/data/tmp"
     read_only = false
     host_path = "/home/kevin/coding/pigallery/tmp/"
+  }
+}
+
+resource "docker_container" "hemmelig" {
+  image = docker_image.hemmelig.image_id
+  name  = "hemmelig"
+  env   = ["SECRET_LOCAL_HOSTNAME=0.0.0.0", "SECRET_PORT=3000"]
+
+  ports {
+    internal = 3000
+    external = 8002
+  }
+
+  volumes {
+    container_path  = "/var/tmp/hemmelig/upload/files"
+    read_only = false
+    host_path = "/home/kevin/coding/hemmelig/data"
+  }
+
+  volumes {
+    container_path  = "/home/node/hemmelig/database/"
+    read_only = false
+    host_path = "/home/kevin/coding/hemmelig/database"
   }
 }
